874505f68e7508e3e98a9f071e83fa2a8d293bdd
nothing2512 <blank345red@gmail.com>
2018-09-01 22:33:26 +0700

    fixed bug almost done

--- app/src/main/java/com/example/robet/rtrain/TrainShow2.java
+++ app/src/main/java/com/example/robet/rtrain/TrainShow2.java
@@ -2,8 +2,10 @@ package com.example.robet.rtrain;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -74,10 +76,18 @@ public class TrainShow2 extends Activity {
         loading.start();
         RestApi.getData().CityList().enqueue(new Callback<CityResponse>() {
             @Override
-            public void onResponse(Call<CityResponse> call, Response<CityResponse> response) {
+            public void onResponse(@NonNull Call<CityResponse> call, @NonNull Response<CityResponse> response) {
                 listCity = response.body().getCity();
                 int size = Integer.valueOf(listCity.size());
-                Toast.makeText(getApplicationContext(), size, Toast.LENGTH_SHORT).show();
+                cityAdapterItem = new String[size];
+
+                for(i = 0; i < size; i++){
+                    cityAdapterItem[i] = String.valueOf(listCity.get(i).getName());
+                }
+                CityAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                        R.layout.list_dialog_item,
+                        cityAdapterItem);
+
             }
 
             @Override
@@ -91,6 +101,16 @@ public class TrainShow2 extends Activity {
             public void onResponse(Call<TimeResponse> call, Response<TimeResponse> response) {
                 loading.stop();
                 listTime = response.body().getTime();
+                int size = Integer.valueOf(listTime.size());
+                timeAdapterItem = new String[size];
+
+                for(i = 0; i < size; i++){
+                    timeAdapterItem[i] = String.valueOf(listTime.get(i).getTime());
+                }
+
+                TimeAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                        R.layout.list_dialog_item,
+                        timeAdapterItem);
             }
 
             @Override
@@ -100,92 +120,104 @@ public class TrainShow2 extends Activity {
             }
         });
 
-//        CityAdapter = new ArrayAdapter<String>(this, R.layout.list_dialog_item,
-//                R.id.tvItem, cityAdapterItem);
-//
-//        TimeAdapter = new ArrayAdapter<String>(this, R.layout.list_dialog_item,
-//                R.id.tvItem, timeAdapterItem);
-
-
     }
 
-//    @OnClick({R.id.btDepart, R.id.btDestination, R.id.btTime, R.id.btBack, R.id.btNext})
-//    public void onViewClicked(View view) {
-//        switch (view.getId()) {
-//            case R.id.btDepart:
+    @OnClick({R.id.btDepart, R.id.btDestination, R.id.btTime, R.id.btBack, R.id.btNext})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btDepart:
 //                showDialog(listView, CityAdapter, 1);
-//                break;
-//            case R.id.btDestination:
-//                showDialog(listView, CityAdapter, 2);
-//                break;
-//            case R.id.btTime:
-//                showDialog(listView, TimeAdapter, 3);
-//                break;
-//            case R.id.btBack:
-//
-//                TrainShow2.this.finish();
-//
-//                break;
-//            case R.id.btNext:
-//
-//                if(depart.isEmpty() && destination.isEmpty() && time.isEmpty()){
-//                    Toast.makeText(getApplicationContext(), "isi data dengan benar", Toast.LENGTH_SHORT).show();
-//                } else {
-//
-//                    String message;
-//                    message = depart + "\n" + destination + "\n" + time;
-//                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-//
-//                    map.put("time", time);
-//                    map.put("destination", destination);
-//                    map.put("depart", depart);
-//
-//                    Intent intent = new Intent(getApplicationContext(), SeatPick.class);
-//                    intent.putExtra("extra", map);
-//                    startActivity(intent);
-//
-//                }
-//
-//                break;
-//        }
-//    }
-//
-//    public void showDialog(ListView mListView, ArrayAdapter<String> arrayAdapter, final int type){
-//
-//        mListView.setAdapter(arrayAdapter);
-//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//
-//                ViewGroup viewGroup = (ViewGroup) view;
-//                TextView tvText = viewGroup.findViewById(R.id.tvItem);
-//                text = tvText.getText().toString();
-//
-//                switch (type){
-//                    case 1:
-//                        depart = text;
-//                        tvDepart.setText(text);
-//                        break;
-//                    case 2:
-//                        destination = text;
-//                        tvDestination.setText(text);
-//                        break;
-//                    case 3:
-//                        time = text;
-//                        tvTime.setText(text);
-//                        break;
-//                }
-//
-//            }
-//        });
-//
-//        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
-//
-//        alertDialogBuilder.setCancelable(true);
-//        alertDialogBuilder.setPositiveButton("ok", null);
+                 AlertDialog.Builder s = new AlertDialog.Builder(this);
+                 s.setCancelable(false);
+                 s.setTitle("quiz");
+                 s.setMessage("benar?");
+                 s.setPositiveButton("y", new DialogInterface.OnClickListener() {
+                     @Override
+                     public void onClick(DialogInterface dialogInterface, int i) {
+                         Toast.makeText(getApplicationContext(), "y", Toast.LENGTH_SHORT).show();
+                     }
+                 })
+                 .setNegativeButton("n", new DialogInterface.OnClickListener() {
+                     @Override
+                     public void onClick(DialogInterface dialogInterface, int i) {
+                         Toast.makeText(getApplicationContext(), "n", Toast.LENGTH_SHORT).show();
+                     }
+                 });
+                 s.create().show();
+                break;
+            case R.id.btDestination:
+                showDialog(listView, CityAdapter, 2);
+                break;
+            case R.id.btTime:
+                showDialog(listView, TimeAdapter, 3);
+                break;
+            case R.id.btBack:
+
+                TrainShow2.this.finish();
+
+                break;
+            case R.id.btNext:
+
+                if(tvDepart.getText().toString().isEmpty() &&
+                        tvDestination.getText().toString().isEmpty() &&
+                        tvTime.getText().toString().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "isi data dengan benar", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    String message;
+                    message = depart + "\n" + destination + "\n" + time;
+                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+                    map.put("time", time);
+                    map.put("destination", destination);
+                    map.put("depart", depart);
+
+                    Intent intent = new Intent(getApplicationContext(), SeatPick.class);
+                    intent.putExtra("extra", map);
+                    startActivity(intent);
+
+                }
+
+                break;
+        }
+    }
+
+    public void showDialog(ListView mListView, ArrayAdapter<String> arrayAdapter, final int type){
+
+        mListView.setAdapter(arrayAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                ViewGroup viewGroup = (ViewGroup) view;
+                TextView tvText = viewGroup.findViewById(R.id.tvItem);
+                text = tvText.getText().toString();
+
+                switch (type){
+                    case 1:
+                        depart = text;
+                        tvDepart.setText(text);
+                        break;
+                    case 2:
+                        destination = text;
+                        tvDestination.setText(text);
+                        break;
+                    case 3:
+                        time = text;
+                        tvTime.setText(text);
+                        break;
+                }
+
+            }
+        });
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
+
+        alertDialogBuilder.setCancelable(true);
+        alertDialogBuilder.setPositiveButton("ok", null);
 //        alertDialogBuilder.setView(mListView);
-//
-//        AlertDialog dialog = alertDialogBuilder.create();
+
+        AlertDialog dialog = alertDialogBuilder.create();
 //        dialog.show();
-//    }
+    }
 }
