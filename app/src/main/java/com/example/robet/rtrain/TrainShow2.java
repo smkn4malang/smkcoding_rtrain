7b6b65503e799fdef6a5e4f371aadf48e0d7bfd5
nothing2512 <blank345red@gmail.com>
2018-09-02 11:41:29 +0700

    still bugg again

--- app/src/main/java/com/example/robet/rtrain/TrainShow2.java
+++ app/src/main/java/com/example/robet/rtrain/TrainShow2.java
@@ -1,24 +1,17 @@
 package com.example.robet.rtrain;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
-import java.util.ArrayList;
+
 import java.util.HashMap;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,38 +22,25 @@ import retrofit2.Response;
 
 public class TrainShow2 extends Activity {
 
-
-    Loading loading;
-    HashMap<String, String> map;
-    Bundle bundle;
-    String time, destination, depart, text, name;
-    ListView listView;
-    int i = 0;
-    List<CityItem> listCity;
-    List<TimeItem> listTime;
-    String[] cityAdapterItem;
-    String[] timeAdapterItem;
-    ArrayAdapter<String> CityAdapter;
-    ArrayAdapter<String> TimeAdapter;
-
     @BindView(R.id.ImgBack)
     ImageView ImgBack;
-    @BindView(R.id.tvDepart)
-    TextView tvDepart;
-    @BindView(R.id.btDepart)
-    LinearLayout btDepart;
-    @BindView(R.id.tvDestination)
-    TextView tvDestination;
-    @BindView(R.id.btDestination)
-    LinearLayout btDestination;
-    @BindView(R.id.tvTime)
-    TextView tvTime;
-    @BindView(R.id.btTime)
-    LinearLayout btTime;
+    @BindView(R.id.etFrom)
+    EditText etFrom;
+    @BindView(R.id.etTo)
+    EditText etTo;
+    @BindView(R.id.spTime)
+    Spinner spTime;
     @BindView(R.id.btBack)
     Button btBack;
     @BindView(R.id.btNext)
     Button btNext;
+
+    Loading loading;
+    HashMap<String, String> map;
+    Bundle bundle;
+    String[] time, destination, depart;
+    ListView listView;
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,17 +57,13 @@ public class TrainShow2 extends Activity {
         RestApi.getData().CityList().enqueue(new Callback<CityResponse>() {
             @Override
             public void onResponse(@NonNull Call<CityResponse> call, @NonNull Response<CityResponse> response) {
-                listCity = response.body().getCity();
-                int size = Integer.valueOf(listCity.size());
-                cityAdapterItem = new String[size];
-
+                int size = Integer.valueOf(response.body().getCity().size());
+                destination = new String[size];
+                depart = new String[size];
                 for(i = 0; i < size; i++){
-                    cityAdapterItem[i] = String.valueOf(listCity.get(i).getName());
+                    destination[i] = String.valueOf(response.body().getCity().get(i).getName());
+                    depart[i] = destination[i];
                 }
-                CityAdapter = new ArrayAdapter<String>(getApplicationContext(),
-                        R.layout.list_dialog_item,
-                        cityAdapterItem);
-
             }
 
             @Override
@@ -100,17 +76,11 @@ public class TrainShow2 extends Activity {
             @Override
             public void onResponse(Call<TimeResponse> call, Response<TimeResponse> response) {
                 loading.stop();
-                listTime = response.body().getTime();
-                int size = Integer.valueOf(listTime.size());
-                timeAdapterItem = new String[size];
-
+                int size = Integer.valueOf(response.body().getTime().size());
+                time = new String[size];
                 for(i = 0; i < size; i++){
-                    timeAdapterItem[i] = String.valueOf(listTime.get(i).getTime());
+                    time[i] = String.valueOf(response.body().getTime().get(i).getTime());
                 }
-
-                TimeAdapter = new ArrayAdapter<String>(getApplicationContext(),
-                        R.layout.list_dialog_item,
-                        timeAdapterItem);
             }
 
             @Override
@@ -122,102 +92,13 @@ public class TrainShow2 extends Activity {
 
     }
 
-    @OnClick({R.id.btDepart, R.id.btDestination, R.id.btTime, R.id.btBack, R.id.btNext})
+    @OnClick({R.id.btBack, R.id.btNext})
     public void onViewClicked(View view) {
         switch (view.getId()) {
-            case R.id.btDepart:
-//                showDialog(listView, CityAdapter, 1);
-                 AlertDialog.Builder s = new AlertDialog.Builder(this);
-                 s.setCancelable(false);
-                 s.setTitle("quiz");
-                 s.setMessage("benar?");
-                 s.setPositiveButton("y", new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialogInterface, int i) {
-                         Toast.makeText(getApplicationContext(), "y", Toast.LENGTH_SHORT).show();
-                     }
-                 })
-                 .setNegativeButton("n", new DialogInterface.OnClickListener() {
-                     @Override
-                     public void onClick(DialogInterface dialogInterface, int i) {
-                         Toast.makeText(getApplicationContext(), "n", Toast.LENGTH_SHORT).show();
-                     }
-                 });
-                 s.create().show();
-                break;
-            case R.id.btDestination:
-                showDialog(listView, CityAdapter, 2);
-                break;
-            case R.id.btTime:
-                showDialog(listView, TimeAdapter, 3);
-                break;
             case R.id.btBack:
-
-                TrainShow2.this.finish();
-
                 break;
             case R.id.btNext:
-
-                if(tvDepart.getText().toString().isEmpty() &&
-                        tvDestination.getText().toString().isEmpty() &&
-                        tvTime.getText().toString().isEmpty()){
-                    Toast.makeText(getApplicationContext(), "isi data dengan benar", Toast.LENGTH_SHORT).show();
-                } else {
-
-                    String message;
-                    message = depart + "\n" + destination + "\n" + time;
-                    Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
-
-                    map.put("time", time);
-                    map.put("destination", destination);
-                    map.put("depart", depart);
-
-                    Intent intent = new Intent(getApplicationContext(), SeatPick.class);
-                    intent.putExtra("extra", map);
-                    startActivity(intent);
-
-                }
-
                 break;
         }
-    }
-
-    public void showDialog(ListView mListView, ArrayAdapter<String> arrayAdapter, final int type){
-
-        mListView.setAdapter(arrayAdapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                ViewGroup viewGroup = (ViewGroup) view;
-                TextView tvText = viewGroup.findViewById(R.id.tvItem);
-                text = tvText.getText().toString();
-
-                switch (type){
-                    case 1:
-                        depart = text;
-                        tvDepart.setText(text);
-                        break;
-                    case 2:
-                        destination = text;
-                        tvDestination.setText(text);
-                        break;
-                    case 3:
-                        time = text;
-                        tvTime.setText(text);
-                        break;
-                }
-
-            }
-        });
-
-        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getApplicationContext());
-
-        alertDialogBuilder.setCancelable(true);
-        alertDialogBuilder.setPositiveButton("ok", null);
-//        alertDialogBuilder.setView(mListView);
-
-        AlertDialog dialog = alertDialogBuilder.create();
-//        dialog.show();
     }
 }
